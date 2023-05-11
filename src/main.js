8007be77e5f4cb2fc7b7f41abe85849293930dae
sphunjam <sphunjam@ucsc.edu>
2023-05-11 12:14:24 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let gameConfig = {
+   scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 1920,
+      height: 1080
+   },
+   scene: [],
+   title: "CMPM 120 - D3"
+};
+
+const game = new Phaser.Game(gameConfig);
